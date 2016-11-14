bec4d3c4a78f275d5b953315842b36abb5c147e2
shubham <it2012.shubham@gmail.com>
2016-11-14 14:10:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubham/edx_project/EdxProjectActivity.java
@@ -0,0 +1,13 @@
+package com.example.shubham.edx_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdxProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ed_xcourse);
+    }
+}
